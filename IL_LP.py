bee4ee9fb6e461a33ecc71878072475842ae484f
Andrey Gorbunov <gor.u.and@gmail.com>
2018-01-29 13:27:45 +0800

    Update IL_LP
    Line by line (corresponding to the updated file):
    
    19 - When we take 'dt' outside the summation side we should multiply it by N, because sum_{i=1}^N dt = N*dt
    27 - I've added N in the denominator by the same reason described in 19
    39 - I've got rid of 'for' cycle

--- IL_LP.py
+++ IL_LP.py
@@ -13,23 +13,38 @@ from scipy import optimize
 
 
 def IL_LP(dt, pr, P, E):
+    # N - the number of variables
     N = len(pr)
-#    c = N * dt * pr
-    c = dt * pr
-    A_eq = (1, N)
-    A_eq = np.ones(A_eq)
-    b_eq = E / dt
-    #ub: upper bound; lb: lower bound
-    ub = np.eye(N, dtype=int)
-    lb = -1 * ub
-    A_ub = np.concatenate((ub, lb))
-    b_ub = (2 * N, 1)
-    b_ub = np.zeros(b_ub)
-    for i in range(N):
-        b_ub[i, 0] = P
-    for i in range(N, 2 * N):
-        b_ub[i, 0] = 0
+    # c - the vector of costs for a LP problem
+    c = N * dt * pr
+#    c = dt * pr
+
+    ### Equality constraints   
+    #  A_eq - the matrix for equality constraints
+    # A_eq - the array of ones corresponding to the total load consumption throughout the scheduling horizon = E
+    A_eq = np.ones((1,N))
+    # b_eq - the vector for equality constraints
+    b_eq = E / (dt*N)
+    
+    ### Inequality constraints
+    ## Upper and lower bound for each variable
+    # bound_up - the upper bound; bound_low - the lower bound
+    bound_up = np.eye(N, dtype=int)
+    bound_low = -1 * bound_up
+    # A_ub - the matrix for inequality constraints
+    # construct the sub-matrix of A_ub corresponding to upper and lower bounds
+    A_ub = np.concatenate((bound_up, bound_low))
+    # b_ub - the vector for inequality constraints
+    # b_bound_up - the vector correspond to upper bounds; b_bound_low - the vector correspond to lower bounds;
+    b_bound_up = P*np.ones((N,1))
+    b_bound_low = np.zeros((N,1))
+    # construct the sub-vector of b_ub corresponding to upper and lower bounds
+    b_up = np.concatenate((b_bound_up, b_bound_low))
+    
+    ## LP solution
     solution = optimize.linprog(c, A_ub, b_ub, A_eq, b_eq, method='simplex')
+    # the value of objective function
     F = solution.fun
+    # the optimal schedule
     x = solution.x
     return F, x
