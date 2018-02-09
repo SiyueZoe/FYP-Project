bae41c1612a2bd030a48355c3ce36e63bab2110a
SiyueZoe <35358115+SiyueZoe@users.noreply.github.com>
2018-02-09 20:32:12 +0800

    Add files via upload

---
+++ NL_MILP.py
@@ -0,0 +1,42 @@
+# NL_MILP - the MILP solution for Non-Interruptable type load
+# Input:
+#      dt - the time step [scalar]
+#      pr - the array of prices from p_b, ..., p_e [array with shape (e-b+1,)]
+#      L - the duration of the task [scalar]
+#      P - the power rate [scalar]
+# Output:
+#      F - the value of objective function [scalar]
+#      x - the optimal schedule [array with shape (e-b+1,)]
+
+from gurobipy import *
+
+## Parameter Setting
+dt = 10
+pr = [100, 100, 5, 90, 5]
+L = 2
+P = 17
+# N - the number of variables
+N = len(pr)
+# x - the list of the variables
+x = [None] * N
+
+## Create a model
+m = Model("NL_MILP")
+# Create variables - x is binary, (P * x) is the actual variable value
+for i in range(N):
+    x[i] = m.addVar(vtype=GRB.BINARY)
+# Integrate new variables
+m.update()
+# Set objective
+m.setObjective(sum(pr[i] * P * x[i] for i in range(N)), GRB.MINIMIZE)
+# Add constraints
+for j in range(1, N - L + 1):
+    m.addConstr(sum(x[i] for i in range(j, j + L)) >= (x[j] - x[j - 1]) * L)
+m.addConstr(sum(x) == L)
+m.addConstr(sum(x[i] for i in range(L)) >= (x[0] * L))
+
+# Optimize
+m.optimize()
+for v in m.getVars():
+    print(v.varName, (v.x) * P)
+print('Obj:', m.objVal)
