7ae9a91072d76cba5c22bb8eb3dd426247e2125d
SiyueZoe <35358115+SiyueZoe@users.noreply.github.com>
2018-02-26 23:44:05 +0800

    Update and rename All_without_Energy_Limit_MILP.py to All_MILP_basic.py

--- All_MILP_basic.py
+++ All_MILP_basic.py
@@ -1,4 +1,4 @@
-# All_without_Energy_Limit_MILP - the MILP solution for Three types of loads without adding energy limit
+# All_MILP_basic - the MILP solution for Three types of loads without adding any extra limit
 # Input:
 #   For All:
 #       dt - the time step [scalar]
